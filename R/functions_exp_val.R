a77811474b3487655812f4a578bfc267f35759ae
FranziskaRuffer <f.f.ruffer@gmail.com>
2025-09-05 10:12:02 +0200

    Added some code.

--- R/functions_exp_val.R
+++ R/functions_exp_val.R
@@ -1,1 +1,59 @@
-# functions to obtain the expected effect sizes given publication bias
+#' Functions to obtain the expected effect sizes given publication bias
+#' Parameter:   
+#' * PB = Publication Bias... proportion of non-significant effect sizes published
+#' * g = true effect size as standardized mean difference in form of Hedges' g
+#' * N = total sample size (N = nT + nC)
+#' * Zcv = critical Z value (used to calculate the critical value ycv)
+#' * vg = sampling variance Hedges' g
+#' * tau2 = (residual) between-study variance 
+#' * I2 = porportion of within- to (residual) between-study variance
+#' * E_sig = Expected effect size upper truncated normal distribution (significant effect sizes)
+#' * E_nsig = Expected effect size lower truncated normal distribution (nonsignificant effect sizes)
+#' * E = expected effect size given publication bias 
+#' * p_ns = probability of a non-significant effect size
+
+
+#' Function to obtain the expected value given publication bias for one true effect size g (Hedges' g)
+exp_val <- function(PB, Zcv, g, N, I2=NA, tau2=NA){
+  
+  #balanced sample size in compared groups (N/2)
+  nT <- nC <- N/2
+  
+  #exact sampling variance Hedges' g ( Viechtbauer (2007, Eq. 22))
+  if(N >120){
+    cm <- 1-(3/(4*(N-2)-1))
+  }else{
+    cm <- gamma((N-2)/2) /
+      (sqrt((N-2)/2) * gamma((N-3)/2))
+  }
+  
+  vg <- ((cm)^2 * (N-2) * 
+           (1 +  nT*nC/(nT + nC) * g^2 )
+  ) / (
+    (N-4) * (nT*nC / (nT + nC))  ) - g^2
+  
+  #critical value
+  ycv <- Zcv*sqrt(vg)
+  
+  #heterogeneity 
+  if(is.na(tau2)==TRUE){
+    tau2 <- (I2 * vg) / (1-I2)
+  } else { I2 <- tau2/(vg + tau2)}
+  
+  #probability of obtaining a non-significant result
+  p_ns <- pnorm((ycv-g)/sqrt(vg+tau2), lower.tail = TRUE) 
+  
+  #expectation significant studies
+  E_sig <- g + sqrt(vg+tau2)* dnorm((ycv-g)/sqrt(vg+tau2)) / pnorm((ycv-g)/sqrt(vg+tau2), lower.tail = FALSE) 
+  
+  #expectation nonsignificant studies
+  E_nsig <- g - sqrt(vg+tau2)* dnorm((ycv-g)/sqrt(vg+tau2)) / pnorm((ycv-g)/sqrt(vg+tau2), lower.tail = TRUE) 
+  
+  #expectation given publication bias
+  E <- (PB * p_ns * E_nsig + (1-p_ns) * E_sig) / 
+    (PB * p_ns + (1-p_ns))
+  
+  return(data.frame(N, vg, tau2, I2, PB, g, ycv, E_sig, E_nsig, E))
+}
+
+
--- R/functions_plotting.R
+++ R/functions_plotting.R
@@ -1,1 +1,32 @@
 # Plotting Functions
+
+p_bias_exp_val <- function(data, title, size_text = 8, axis_text = 23){
+  ggplot(data=data,aes(x=g, y=bias, linetype=PB)) + 
+    geom_line(linewidth=1.2) +
+    labs( x = expression(theta[i]) , 
+          y= "Bias",
+          title = title) +
+    scale_linetype_manual(values = 1:5,
+                          labels = c("0", "0.05", "0.2", "0.5", "1"),
+                          name = "Proportion of published non-signigicant studies [PB]") +
+    theme(legend.text = element_text(size = axis_text-4),
+          legend.key.size = unit(3,"line"),
+          legend.title = element_text(size = axis_text-4),  # Adjust vertical position of legend title
+          legend.position = "bottom",           # Position legend at the bottom
+          title = element_text(size = axis_text-4),
+          panel.grid.major = element_blank(),  # Remove major grid lines
+          panel.grid.minor = element_blank(),  # Remove minor grid lines
+          plot.background = element_rect(fill = "white"),  # Set plot background color to white
+          panel.background = element_rect(fill = "white"),  # Set panel background color to white
+          panel.border = element_rect(color = "black", fill = NA),  # Set panel border color to black
+          plot.margin = margin(10, 15, 10, 10, "pt"),
+          axis.title.x = element_text(color = "black", size=axis_text-2),
+          axis.title.y = element_text(color = "black", size=axis_text-2),
+          axis.text.y = element_text(size=axis_text-2),
+          axis.text.x = element_text(size=axis_text-2))  + # Add margin around the plot
+    guides(linetype = guide_legend(title.position = "top"),
+           colour = guide_legend(title.position = "top"))  + # Place legend title on top
+    scale_x_continuous(limits = c(0, 0.8), breaks = seq(0, 0.8, by = 0.1), expand=c(0,0)) +  # Customize x-axis ticks
+    scale_y_continuous(
+      limits = c(-0.01, 1), breaks = seq(0, 1, by = 0.1), expand=c(0,0)) 
+}
